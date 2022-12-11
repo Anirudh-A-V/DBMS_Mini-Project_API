2f68c0856cd0e91a2d46af8e06932a3e3b9a30e8
Anirudh A V <anirudh.av02@gmail.com>
2022-12-11 18:43:25 +0530

    Serializers added fror consumer app

---
+++ Electricity_Billing_API/consumer/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from .models import Consumer, Bill, BillStatus, Complaint
+
+class ConsumerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Consumer
+        fields = "__all__"
+
+class BillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bill
+        fields = "__all__"
+
+class BillStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BillStatus
+        fields = "__all__"
+
+class ComplaintSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Complaint
+        fields = "__all__"
+
+        
