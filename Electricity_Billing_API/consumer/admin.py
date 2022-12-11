7de646b6097fd16b9b967e29b6ece134df41e716
Anirudh A V <anirudh.av02@gmail.com>
2022-12-11 18:26:36 +0530

    Consumer Models registered

--- Electricity_Billing_API/consumer/admin.py
+++ Electricity_Billing_API/consumer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Consumer, Bill, BillStatus, Complaint
 # Register your models here.
+
+admin.site.register(Consumer)
+admin.site.register(Bill)
+admin.site.register(BillStatus)
+admin.site.register(Complaint)
