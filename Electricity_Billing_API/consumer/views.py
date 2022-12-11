b24d51cd106dac37688ef1ccce9349f457944364
Anirudh A V <anirudh.av02@gmail.com>
2022-12-11 19:31:13 +0530

    Consumer viewsets defined

--- Electricity_Billing_API/consumer/views.py
+++ Electricity_Billing_API/consumer/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
 # Create your views here.
+
+from .models import Consumer, Bill, BillStatus, Complaint
+from .serializers import ConsumerSerializer, BillSerializer, BillStatusSerializer, ComplaintSerializer
+
+class ConsumerViewSet(viewsets.ModelViewSet):
+    queryset = Consumer.objects.all()
+    serializer_class = ConsumerSerializer
+
+class BillViewSet(viewsets.ModelViewSet):
+    queryset = Bill.objects.all()
+    serializer_class = BillSerializer
+
+class BillStatusViewSet(viewsets.ModelViewSet):
+    queryset = BillStatus.objects.all()
+    serializer_class = BillStatusSerializer
+
+class ComplaintViewSet(viewsets.ModelViewSet):
+    queryset = Complaint.objects.all()
+    serializer_class = ComplaintSerializer
+    
